73ad1c8a31c70cd4fd46951ed8a3dc7f79c46dac
Luke Franzke <lukefranzke@gmail.com>
2018-10-12 14:19:31 +0200

    Settings for using with Carvey CNC

--- settings/pcb-defaults.h
+++ settings/pcb-defaults.h
@@ -1,19 +1,19 @@
 //
 // Default values for generating gcode from a PCB.
 //
-// These settings were last changed with pcb-gcode-setup: 7/8/2017 1:18 PM
+// These settings were last changed with pcb-gcode-setup: 12/10/18 14:08
 //
 //
 // Changes you make in this file will be overwritten if you use pcb-gcode-setup.
 //
 
 int SINGLE_PASS = NO;
-real ISO_MIN = 0.025400;
-real ISO_MAX = 0.500000;
+real ISO_MIN = 0.010000;
+real ISO_MAX = 0.100000;
 real ISO_STEP = 0.100000;
 
-int GENERATE_TOP_OUTLINES = YES;
-int GENERATE_TOP_DRILL = YES;
+int GENERATE_TOP_OUTLINES = NO;
+int GENERATE_TOP_DRILL = NO;
 int GENERATE_TOP_FILL = NO;
 int GENERATE_TOP_STENCIL = NO;
 
@@ -25,12 +25,12 @@ int GENERATE_BOTTOM_STENCIL = NO;
 int MIRROR_BOTTOM = NO;
 int SIMPLE_DRILL_CODE = NO;
 
-int GENERATE_MILLING = YES;
+int GENERATE_MILLING = NO;
 int CLIMB_MILLING = YES;
 
 int GENERATE_TEXT = NO;
 
-int SPOT_DRILL = YES;
+int SPOT_DRILL = NO;
 real SPOT_DRILL_DEPTH = -0.254000;
 
 int DO_TOOL_CHANGE_WITH_ZERO_STEP = NO;
